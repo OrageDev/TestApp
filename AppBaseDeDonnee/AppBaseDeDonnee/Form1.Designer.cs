2568389aa1fd83fa7d2285a80d638e64978e46b7
Orage <clyde.biddle@viacesi.fr>
2016-01-09 19:23:47 +0100

    Test

---
+++ AppBaseDeDonnee/AppBaseDeDonnee/Form1.Designer.cs
@@ -0,0 +1,165 @@
+﻿namespace AppBaseDeDonnee
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btn_select = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btn_insert = new System.Windows.Forms.Button();
+            this.txtbox_nom = new System.Windows.Forms.TextBox();
+            this.txtbox_prenom = new System.Windows.Forms.TextBox();
+            this.btn_delete = new System.Windows.Forms.Button();
+            this.checkbox_delete = new System.Windows.Forms.CheckBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btn_select
+            // 
+            this.btn_select.Location = new System.Drawing.Point(441, 198);
+            this.btn_select.Name = "btn_select";
+            this.btn_select.Size = new System.Drawing.Size(100, 23);
+            this.btn_select.TabIndex = 0;
+            this.btn_select.Text = "SELECT ALL";
+            this.btn_select.UseVisualStyleBackColor = true;
+            this.btn_select.Click += new System.EventHandler(this.btn_select_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(351, 229);
+            this.dataGridView1.TabIndex = 1;
+            // 
+            // btn_insert
+            // 
+            this.btn_insert.Location = new System.Drawing.Point(441, 101);
+            this.btn_insert.Name = "btn_insert";
+            this.btn_insert.Size = new System.Drawing.Size(100, 22);
+            this.btn_insert.TabIndex = 2;
+            this.btn_insert.Text = "INSERT";
+            this.btn_insert.UseVisualStyleBackColor = true;
+            this.btn_insert.Click += new System.EventHandler(this.btn_insert_Click);
+            // 
+            // txtbox_nom
+            // 
+            this.txtbox_nom.Location = new System.Drawing.Point(441, 49);
+            this.txtbox_nom.Name = "txtbox_nom";
+            this.txtbox_nom.Size = new System.Drawing.Size(100, 20);
+            this.txtbox_nom.TabIndex = 3;
+            // 
+            // txtbox_prenom
+            // 
+            this.txtbox_prenom.Location = new System.Drawing.Point(441, 75);
+            this.txtbox_prenom.Name = "txtbox_prenom";
+            this.txtbox_prenom.Size = new System.Drawing.Size(100, 20);
+            this.txtbox_prenom.TabIndex = 4;
+            // 
+            // btn_delete
+            // 
+            this.btn_delete.Enabled = false;
+            this.btn_delete.Location = new System.Drawing.Point(441, 127);
+            this.btn_delete.Name = "btn_delete";
+            this.btn_delete.Size = new System.Drawing.Size(100, 24);
+            this.btn_delete.TabIndex = 5;
+            this.btn_delete.Text = "DELETE";
+            this.btn_delete.UseVisualStyleBackColor = true;
+            this.btn_delete.Click += new System.EventHandler(this.btn_delete_Click);
+            // 
+            // checkbox_delete
+            // 
+            this.checkbox_delete.AutoSize = true;
+            this.checkbox_delete.Location = new System.Drawing.Point(418, 157);
+            this.checkbox_delete.Name = "checkbox_delete";
+            this.checkbox_delete.Size = new System.Drawing.Size(114, 17);
+            this.checkbox_delete.TabIndex = 6;
+            this.checkbox_delete.Text = "Mode Suppression";
+            this.checkbox_delete.UseVisualStyleBackColor = true;
+            this.checkbox_delete.CheckedChanged += new System.EventHandler(this.checkbox_delete_CheckedChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(379, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(267, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Entrer le nom et le prenom de la personne a enregistrer.";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(580, 49);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(207, 102);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 8;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(787, 259);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.checkbox_delete);
+            this.Controls.Add(this.btn_delete);
+            this.Controls.Add(this.txtbox_prenom);
+            this.Controls.Add(this.txtbox_nom);
+            this.Controls.Add(this.btn_insert);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.btn_select);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "MaBaseDeDonnée_test";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_select;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btn_insert;
+        private System.Windows.Forms.TextBox txtbox_nom;
+        private System.Windows.Forms.TextBox txtbox_prenom;
+        private System.Windows.Forms.Button btn_delete;
+        private System.Windows.Forms.CheckBox checkbox_delete;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ AppBaseDeDonnee/AppBaseDeDonnee/Form1.cs
@@ -0,0 +1,120 @@
+﻿//Created By Orage
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AppBaseDeDonnee
+{
+    public partial class Form1 : Form
+    {
+        private string rq_sql;                              //Objet string qui contiendra les requêtes SQL
+        private string cnx;                                 //Object string qui contiendra la clé de connection a la BDD
+        private System.Data.OleDb.OleDbConnection oCNX;     //Objet Connection de BDD
+        private System.Data.OleDb.OleDbCommand oCMD;        //Objet Command qui executera une requête SQL
+        private System.Data.OleDb.OleDbDataAdapter oDA;     //Objet DataAdapter qui fait le lien entre l'application et la BDD
+        private System.Data.DataSet oDS;                    //Objet qui stock en local les informations de la BDD
+
+        public Form1()
+        {
+            InitializeComponent();
+            Form1_Load();
+            
+        }
+
+        //##########################################################
+        //#            Changer l'emplacement de la BDD             #
+        //##########################################################
+        private void Form1_Load()
+        {
+            this.rq_sql = null;
+                                                                          //Changer l'emplacement ou ce trouve votre BDD
+            this.cnx = @"Provider = Microsoft.Jet.OLEDB.4.0; Data Source = C:\Users\Orage\Documents\Visual Studio 2015\Projects\AppBaseDeDonnee\AppBaseDeDonnee\MyBDD.mdb";
+            this.oCNX = new System.Data.OleDb.OleDbConnection(this.cnx);  
+            this.oCMD = null;
+            this.oDA = null;
+            this.oDS = new DataSet();
+            this.m_select();
+        }
+
+        private void m_select()
+        {
+            this.oDS = new DataSet();
+            this.rq_sql = "SELECT * FROM TB_PERSONNE;";                              //Requête SELECT Paramétrée
+            this.oCMD = new System.Data.OleDb.OleDbCommand(this.rq_sql, this.oCNX);  //Création d'un objet commande qui prend en paramètre la requête SQL et la l'objet de Connexion a la BDD.
+            this.oDA = new System.Data.OleDb.OleDbDataAdapter(this.oCMD);            //Création d'un objet DataAdapter qui prend en paramètre l'objet commande. (DataAdapter et l'interface réel entre l'application et la BDD.
+            this.oDA.Fill(this.oDS, "personne");                                     //Execution de la requête SQL, si retournes des enregistrements, elle sont placé dans l'objet DataSet, dans la DataTable "Personne".
+            this.dataGridView1.DataSource = this.oDS;                                //On attribue le modèle de mon DataSet au gridview pour afficher mes enregistrement.
+            this.dataGridView1.DataMember = "personne";                              //Mais bien définir la table a afficher, car le DataSet peut en contenir plusieur.
+        }
+
+        //Mode Personnel de suppression.
+        private void checkbox_delete_CheckedChanged(object sender, EventArgs e)
+        {
+
+            
+            if(checkbox_delete.Checked == true) // Si Actif.
+            {
+                btn_delete.Enabled = true; //Active le bouton Delete.
+                this.label1.Text = "Entrer le nom de la personne a supprimer."; // Change le Label.
+                btn_insert.Enabled = false; //Désactive le bouton Insert.
+            }
+            if(checkbox_delete.Checked == false) // Si non Actif.
+            {
+                btn_delete.Enabled = false; // Desactive le bouton Delete.
+                this.label1.Text = "Entrer le nom et le prenom de la personne a enregistrer."; // Change le Label.
+                btn_insert.Enabled = true; // Active le bouton Insert
+            }
+        }
+
+        private void btn_select_Click(object sender, EventArgs e)
+        {
+            this.m_select();
+        }
+
+        private void btn_insert_Click(object sender, EventArgs e)
+        {
+
+            if (this.txtbox_nom.Text == "" || this.txtbox_prenom.Text == "")
+            {
+                MessageBox.Show("Veuillez entrer le NOM et le PRENOM de la personne que vous voulez ajouter.");
+            }
+            else
+            {
+                this.rq_sql = "INSERT INTO TB_PERSONNE ([Nom_personne], [Prenom_personne]) VALUES ('" + this.txtbox_nom.Text + "','" + this.txtbox_prenom.Text + "');";
+                this.oCMD = new System.Data.OleDb.OleDbCommand(this.rq_sql, this.oCNX);
+                this.oCNX.Open();
+                this.oCMD.ExecuteNonQuery();
+                this.oCNX.Close();
+                MessageBox.Show("Votre utilisateur a été correctement ajouté.");
+                this.m_select();
+
+            }
+        }
+
+        private void btn_delete_Click(object sender, EventArgs e)
+        {
+            if (this.txtbox_nom.Text == "" || this.txtbox_prenom.Text == "")
+            {
+                MessageBox.Show("Veuillez entrer le NOM et le PRENOM de la personne que vous voulez supprimer");
+            }
+            else
+            {
+                this.rq_sql = "DELETE FROM `TB_PERSONNE` WHERE `Nom_personne` = '" + this.txtbox_nom.Text + "' AND `Prenom_personne` = '" + this.txtbox_prenom.Text + "'";
+                this.oCMD = new System.Data.OleDb.OleDbCommand(this.rq_sql, this.oCNX);
+                this.oCNX.Open();
+                this.oCMD.ExecuteNonQuery();
+                this.oCNX.Close();
+                MessageBox.Show("Votre utilisateur a été correctement supprimé.");
+                this.m_select();
+            }
+        }
+
+    }
+}
